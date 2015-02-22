947e519dbc4876220c6e31afbce7337b830f64ce
Santiago Cortes <scortesie@gmail.com>
2015-02-22 16:50:08 +0000

    Adding utf-8 support

--- src/subtitools/processing/filter.py
+++ src/subtitools/processing/filter.py
@@ -1,6 +1,10 @@
 import re
+import logging
 
 from src.subtitools.processing.subtitle import Subtitle
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 class Filter(object):
@@ -14,7 +18,7 @@ class Filter(object):
 class HideTextFilter(Filter):
     def __init__(self, percentage_to_hide):
         Filter.__init__(self)
-        self.re_word = re.compile('\w+')
+        self.re_word = re.compile('\w+', re.UNICODE)
         self.percentage_to_hide = int(percentage_to_hide)
         if self.percentage_to_hide not in range(1, 101):
             raise ValueError("Percentage must be a number between 1 and 100")
@@ -23,17 +27,21 @@ class HideTextFilter(Filter):
         return '_' * len(word)
 
     def filter(self, subtitle):
+        subtitle_text = subtitle.text.decode('utf-8')
         subtitle_text_filtered = ''
         text_current_char_index = 0
         text_current_word_position = 0
-        for match in self.re_word.finditer(subtitle.text):
+        for match in self.re_word.finditer(subtitle_text):
             text_current_word_str = match.group()
             text_current_word_span = match.span()
-            subtitle_text_filtered += subtitle.text[text_current_char_index:text_current_word_span[0]]
+            subtitle_text_filtered += subtitle_text[text_current_char_index:text_current_word_span[0]]
             if text_current_word_position % int(100 / self.percentage_to_hide) == 0:
                 text_current_word_str = self.hide_word(text_current_word_str)
             subtitle_text_filtered += text_current_word_str
             text_current_char_index = text_current_word_span[1]
             text_current_word_position += 1
-        subtitle_text_filtered += subtitle.text[text_current_char_index:]
-        return Subtitle(subtitle.identifier, subtitle.timestamp_begin, subtitle.timestamp_end, subtitle_text_filtered)
+        subtitle_text_filtered += subtitle_text[text_current_char_index:]
+        return Subtitle(subtitle.identifier,
+                        subtitle.timestamp_begin,
+                        subtitle.timestamp_end,
+                        subtitle_text_filtered.encode('utf-8'))
