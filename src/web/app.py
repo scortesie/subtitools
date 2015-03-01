e963f91c80900113d75c0c92894e36742b58998a
Santiago Cortes <scortesie@gmail.com>
2015-03-01 17:10:40 +0000

    Adding tune subtitles web form and controller action

---
+++ src/web/app.py
@@ -0,0 +1,30 @@
+from StringIO import StringIO
+
+import flask
+
+from src.subtitools.processing.tuner import Tuner
+from src.subtitools.processing.srtwriter import SrtWriter
+
+
+app = flask.Flask(__name__, static_folder='./static/')
+
+
+@app.route('/')
+@app.route('/<path:filename>')
+def serve_static(filename='index.html'):
+    return app.send_static_file(filename)
+
+
+@app.route('/tune_subtitles/', methods=['POST'])
+def tune_subtitles():
+    file_srt_to_tune = flask.request.files['file']
+
+    tuner = Tuner()
+    subtitles_tuned = tuner.tune(file_srt_to_tune)
+
+    file_srt_tuned = StringIO()
+    writer = SrtWriter(file_srt_tuned)
+    writer.write_subtitles(subtitles_tuned)
+    file_srt_tuned.seek(0)
+
+    return flask.send_file(file_srt_tuned, attachment_filename=file_srt_to_tune.filename, as_attachment=True)
